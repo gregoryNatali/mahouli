49c12c3bf2f38f0046034a06c5a9c4d5d204822c
NomikuH8 <sofaco.checkin@gmail.com>
2022-08-04 22:18:56 -0300

    setup server and client

---
+++ server/src/data-source.ts
@@ -0,0 +1,17 @@
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+import "reflect-metadata"
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3307,
+    username: "root",
+    password: "",
+    database: "mahouli",
+    synchronize: true,
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+})
---
+++ server/src/entity/User.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column("varchar")
+    profile_picture: string
+
+    @Column("varchar", { length: 50 })
+    nickname: string
+
+    @Column("varchar", { length: 320 })
+    email: string
+
+    @Column("boolean")
+    confirmed_email: boolean
+
+    @Column("varchar", { length: 320 })
+    password: string
+
+    @Column("date")
+    join_date: Date
+
+}
