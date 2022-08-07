b75093b2998656f01ad5c2ea5a75c33464c659bd
NomikuH8 <sofaco.checkin@gmail.com>
2022-08-06 22:39:38 -0300

    configured vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// setting proxy to other port
+// is useful to make requests
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://localhost:8080',
+				secure: false,
+			}
+		}
+	}
 })
