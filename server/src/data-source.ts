f6ec64e9b21dc6f45cc0f5ca44e2997f9aff23a6
NomikuH8 <sofaco.checkin@gmail.com>
2022-08-09 03:34:00 -0300

    fixed indentation and changed values to work in home

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,15 +3,15 @@ import { User } from "./entity/User"
 import "reflect-metadata"
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3307,
-    username: "root",
-    password: "",
-    database: "mahouli",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
+	type: "mysql",
+	host: "localhost",
+	port: 3306,
+	username: "root",
+	password: "root",
+	database: "mahouli",
+	synchronize: true,
+	logging: false,
+	entities: [User],
+	migrations: [],
+	subscribers: [],
 })
