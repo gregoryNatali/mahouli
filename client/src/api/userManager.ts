9c8edb0cecebf7b5b1cc86c0ba1efdb54a7f4416
NomikuH8 <sofaco.checkin@gmail.com>
2022-11-12 15:37:16 -0300

    actually fixed merge conflicts now

--- client/src/api/userManager.ts
+++ client/src/api/userManager.ts
@@ -4,7 +4,6 @@ const baseUrl = 'http://localhost:8080/api'
 
 
 export async function sendLogin(email: string, password: string, redirect: NavigateFunction) {
-<<<<<<< HEAD
 	const req = await fetch(`${baseUrl}/user/login`, {
 		method: 'POST',
 		body: JSON.stringify({
@@ -22,23 +21,6 @@ export async function sendLogin(email: string, password: string, redirect: Navig
 					token: data.token,
 				}
 			})
-=======
-  const req = await fetch(`${baseUrl}/user/login`, {
-    method: 'POST',
-    body: JSON.stringify({
-      email,
-      password
-    })
-  })
-
-  const data = await req.json()
-  localStorage.setItem('token', data.token)
-  console.log(data)
-
-  if (data.success === false) {
-    if (data.message === "email not confirmed") {
-      redirect('/email-confirmation')
->>>>>>> 3476b2afa64ab762b3699594a7f70e1c0ab4cee3
       return
     }
 
@@ -66,15 +48,9 @@ export async function sendRegister(name: string, email: string, password: string
   redirect('/signin')
 }
 
-<<<<<<< HEAD
 export async function sendEmailConfirm(confirm_code: string, token: string, redirect: NavigateFunction) {
 	const headers = new Headers()
 	headers.set('Authorization', token)
-=======
-export async function sendEmailConfirm(confirm_code: string, redirect: NavigateFunction) {
-  const headers = new Headers()
-  headers.set('Authorization', localStorage.getItem('token')!)
->>>>>>> 3476b2afa64ab762b3699594a7f70e1c0ab4cee3
 
   const req = await fetch(`${baseUrl}/user/confirm-email`, {
     headers,
