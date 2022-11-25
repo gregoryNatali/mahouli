0db7efb4f40df52e6799a4be138f3931db813ed8
JOAO MIGUEL DE CASTRO MENNA <joao_menna@estudante.sc.senai.br>
2022-11-25 08:14:44 -0300

    fixed shit

--- client/src/pages/Account/index.tsx
+++ client/src/pages/Account/index.tsx
@@ -124,7 +124,7 @@ export function AccountPage() {
             <div className="carousel-wrapper">
               {favMangas.length > 0
                 ?
-                <Carousel slidesToShow={4} style={{ overflowX: 'clip', maxWidth: '80vw' }}>
+                <Carousel slidesToShow={4} style={{ overflowX: 'clip' }}>
                   {favMangas.map((entry, idx) =>
                     <img
                       key={idx}
